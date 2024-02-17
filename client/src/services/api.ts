f813d70199b3a9b019933d50352a9658294a2806
shelcia <shelcia1999@gmail.com>
2024-02-16 20:52:09 -0600

    🐛 deploy fix

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL: string = "http://moral-web-backend.vercel.app/api";
+export const BACKEND_URL: string = "https://moral-web-backend.vercel.app/api";
 
 export const LOCALHOST_URL: string = "http://localhost:8000/api";
